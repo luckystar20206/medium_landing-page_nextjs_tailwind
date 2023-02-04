78c1eeab3dfb6d75e67fd65526e01e83e4208a04
luckystar20206 <luckystar20206@gmail.com>
2023-02-04 14:32:53 +0800

    header background

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,13 @@ import React from "react"
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className="header header-bg border-b border-black">
             <div className="main-container p-20">
-                <div className=" w-3/6">
+                <div className="w-4/5 md:w-3/6">
                     <h1 className="font-serif font-medium text-8xl mb-4">Stay curious.</h1>
                     <h2 className="font-sans font-normal text-3xl mb-10">Discover stories, thinking, and expertise from writers on any topic.</h2>
                     <button className="btn-rounded-md-black">Start Reading</button>
                 </div>
-            </div>
-            <div>
-
             </div>
         </header>
     )
